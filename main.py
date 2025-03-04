fc56bab6dc5e8f22b269dbfb909901856b76ebdc
Obi.M <mail@obimadu.pro>
2025-03-04 17:49:37 +0100

    feat: commit that succeeds - Hello BackendIM

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello BackendIM"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
