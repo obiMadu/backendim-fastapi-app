8abb01f120b1abef6f9f43945a25355f21521052
Obi.M <mail@obimadu.pro>
2025-03-04 16:02:56 +0100

    feat: commit that succeeds - Hello Mark

---
+++ tests/test_success.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Mark"}
+
+
+def test_read_item():
+    response = client.get("/items/42")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 42, "message": "Item details"}
